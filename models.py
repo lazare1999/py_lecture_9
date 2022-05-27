6a069d31c8c4b3fe5d9aed88baab47f84dc2fe17
lazo <lazarekvirtia@gmail.com>
2022-05-27 22:28:17 +0400

    initial commit

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Boolean, Integer, Text, String, Column
+
+from database import Base
+
+
+class Item(Base):
+    __table_args__ = {'extend_existing': True}
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
+    description = Column(Text)
+    price = Column(Integer, nullable=False)
+    on_offer = Column(Boolean, default=False)
